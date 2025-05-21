c62bc940c7b84070e1303b0f1cf522f86315975d
name-q <1620206666@qq.com>
2025-05-21 16:50:54 +0800

    paths

--- demo.mjs
+++ demo.mjs
@@ -1,9 +1,5 @@
-import { playVideo } from './dist/index.js';
-import path from 'node:path';
+import { playVideo } from "./dist/index.js";
 
-const filePath = path.resolve('./video.mp4'); // 放你的视频路径
-
-await playVideo(filePath, {
-  fps: 10,
-  width: 80,
-});
+(async () => {
+  await playVideo("./video.mp4");
+})();
